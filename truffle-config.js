ffe7017316196284ceba1e14271962efbd6b3f0d
sanjay3696 <66181363+sanjay3696@users.noreply.github.com>
2021-07-29 20:10:06 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,19 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },testnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
   },
   contracts_directory: './src/contracts/',
